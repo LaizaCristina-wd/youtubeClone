7dda37895bc0db31b121b008c8942e3ddfe78301
laiza <laizacristina2507@gmail.com>
2026-03-09 04:04:31 -0300

    menus em javascript

--- src/app.js
+++ src/app.js
@@ -4,4 +4,38 @@ const input = document.querySelector(".search-input");
 btn.addEventListener("click", () => {
   input.classList.toggle("active")
   input.focus()
-})
+});
+
+const buttonToggle = document.querySelector(".menu");
+const menuToggle = document.querySelector(".menu-toggle");
+const content = document.querySelector(".video-container");
+const background = document.querySelector(".background");
+
+buttonToggle.addEventListener("click", () => {
+ menuToggle.classList.toggle("active")
+ buttonToggle.classList.toggle("active")
+ content.classList.toggle("active")
+ background.classList.toggle("active")
+ document.body.style.backgroundColor = menuToggle.classList.contains("active") ? "rgb(8, 8, 8)" :  "rgb(8, 8, 8)"
+});
+
+  background.addEventListener("click", () => {
+  menuToggle.classList.remove("active")
+  buttonToggle.classList.remove("active")
+  content.classList.remove("active")
+  background.classList.remove("active")
+   document.body.style.backgroundColor =  "rgb(8, 8, 8)" 
+});
+
+const moreBottom = document.querySelector(".more-mobile");
+const menuBottom = document.querySelector(".box-more");
+
+moreBottom.addEventListener("click", () => {
+ menuBottom.classList.toggle("active")
+ moreBottom.classList.toggle("active")
+ 
+});
+moreBottom.addEventListener("click", () => {
+ menuBottom.classList.remove("active")
+ moreBottom.classList.remove("active")
+});
